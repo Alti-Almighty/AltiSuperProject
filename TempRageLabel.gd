8669f315b2a38c928b1191646d82fc7f79dfa38b
Adrian Jurczak <ajurczak@altimetrik.com>
2022-01-26 22:01:47 +0100

    rage timer logic

---
+++ TempRageLabel.gd
@@ -0,0 +1,24 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Node2D_timeout():
+	if self.is_visible():
+		self.hide()
+	else:
+		self.show()
+	var a = get_node("../Timer").start(3)
