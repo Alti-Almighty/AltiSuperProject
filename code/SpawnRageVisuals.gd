abd2f6c5621f11887c9014bc87ee76696da2f657
Adrian Jurczak <ajurczak@altimetrik.com>
2022-01-27 18:33:41 +0100

    adding camera that follows player

---
+++ code/SpawnRageVisuals.gd
@@ -0,0 +1,23 @@
+extends Node
+
+const RageProgressBar = preload("res://prefabs/timer/RageTimer.tscn")
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Timer_timeout():
+	#var ins = RageProgressBar.instance()
+	#add_child(ins)
+	pass
