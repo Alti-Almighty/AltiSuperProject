660c73386942afd8f106407ebb68c98ed504af3b
Jay <jarek.wawro@gmail.com>
2022-01-27 21:45:52 +0100

    Fix to the texture size based on the viewport

--- code/RageTextureIgnoreRot.gd
+++ code/RageTextureIgnoreRot.gd
@@ -6,7 +6,9 @@ extends TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#hook to the window's main resize event.
+	rect_pivot_offset = Vector2(get_viewport().size.x / 2, get_viewport().size.y / 2)
+	get_tree().get_root().connect("size_changed", self, "viewport_changed")	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,3 +18,7 @@ func _process(delta):
 	self.set_position(camera_position)
 	var viewport_size = get_viewport().size
 	self.set_size( Vector2(viewport_size.x, viewport_size.y) )
+	
+
+func viewport_changed():
+	rect_pivot_offset = Vector2(get_viewport().size.x / 2, get_viewport().size.y / 2)
