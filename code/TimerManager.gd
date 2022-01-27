6a9e2b7355db139a0ab756f292d663163b900506
Adrian Jurczak <ajurczak@altimetrik.com>
2022-01-27 18:07:01 +0100

    slowing rage progress, adding time manager to trigger rage and removing old scene

--- code/TimerManager.gd
+++ code/TimerManager.gd
@@ -1,10 +1,6 @@
-extends Area2D
+extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+const Projectile = preload("res://prefabs/timer/RageTimer.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,4 +10,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
