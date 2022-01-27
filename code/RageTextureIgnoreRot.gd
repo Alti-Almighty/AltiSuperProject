2084c66fbf14bcafa9360f33f3123c1c8fa24b19
Adrian Jurczak <ajurczak@altimetrik.com>
2022-01-27 19:10:08 +0100

    rage texture blended with camera

---
+++ code/RageTextureIgnoreRot.gd
@@ -0,0 +1,19 @@
+extends TextureRect
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var camera_position = $"../KinematicBody2D".position
+	camera_position.x -= get_viewport_rect().size.x/2
+	camera_position.y -= get_viewport_rect().size.y/2
+	self.set_position(camera_position)
