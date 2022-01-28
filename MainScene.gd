6b1b75c2b0767fa83552351e84c0e9b5843a1cf5
Adrian Jurczak <ajurczak@altimetrik.com>
2022-01-28 16:15:29 +0100

    randomly spawn powerups

--- MainScene.gd
+++ MainScene.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const OmiPowerUp = preload("res://prefabs/collectibles/OmicronCollectible.tscn")
+const SyrPowerUp = preload("res://prefabs/collectibles/SyringeCollectible.tscn")
 
 func _ready():
 	Session.spawn_players()
@@ -8,8 +9,10 @@ func _ready():
 	#$RageTimer.start_timer()	
 	$EffectsController.restart()
 	_play_music()
-	var ins = OmiPowerUp.instance()
-	add_child(ins)
+	
+	powerup_random_spawn(OmiPowerUp)
+	powerup_random_spawn(SyrPowerUp)
+
 
 func _on_RageTimer_timer_kaboom():
 	$EffectsController.kaboom()
@@ -18,3 +21,11 @@ func _on_RageTimer_timer_kaboom():
 func _play_music():
 	SoundMixer.playChill()
 
+func powerup_random_spawn(res: Resource):
+	var ins = res.instance()
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var rand_x = rng.randi_range(0, get_viewport_rect().size.x)
+	var rand_y = rng.randi_range(0, get_viewport_rect().size.y)
+	ins.transform.origin = Vector2(rand_x, rand_y)
+	add_child(ins)
