72849c29cad6608ae826ed1ea3f878c1da2ca9fb
Paweł Bis <aoame@Pawes-MacBook-Air.local>
2022-01-30 01:53:35 +0100

    Add single player mode

--- code/networking/Lobby.gd
+++ code/networking/Lobby.gd
@@ -3,19 +3,28 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_player_connected")
-	
 
-func _player_connected(id):
-	print("Player " + str(id) + " connected to server!")
-	Session.player_ids.push_back(id)
+
+func create_game_map():
 	var game = preload("res://MainScene.tscn").instance()
 	game.set_name("Game")
 	get_tree().get_root().add_child(game)
 
+func _player_connected(id):
+	print("Player " + str(id) + " connected to server!")
+	Session.player_ids.push_back(id)
+	create_game_map()
+
 func hide_ui():
+	$SinglePlayer.hide()
 	$HostLobbyBtn.hide()
 	$JoinLobbyBtn.hide()
 	$JoinLobbyIp.hide()
+
+func _on_single_player_pressed():
+	_on_host_lobby_pressed()
+	hide_ui()
+	create_game_map()
 
 func _on_host_lobby_pressed():
 	print("Hosting lobby")
